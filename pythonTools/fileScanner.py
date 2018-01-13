3608b1a8163c5d0138c4be64d27afbe54b37bbd0
smallpi <smallpi.yuewen@gmail.com>
2018-01-13 16:44:47 +0800

    modify fileScanner.py

--- pythonTools/fileScanner.py
+++ pythonTools/fileScanner.py
@@ -23,6 +23,10 @@ class FileScanner(object):
         
         self.__file = None
 
+        self.__bPrintLineNum = True
+        self.__bPrintLineContent = True
+        self.__filenameFound = []
+
     def setPath(self,path):
         self.__path = path
 
@@ -35,6 +39,11 @@ class FileScanner(object):
         self.__bSave = bSave
         self.__saveFile = saveFile
 
+    def setPrintLineNum(self,bPrintLineNum):
+        self.__bPrintLineNum = bPrintLineNum
+
+    def setPrintLineContent(self,bPrintLineContent):
+        self.__bPrintLineContent = bPrintLineContent
 
     def scan(self):
         if not self.__beforeScan():
@@ -55,15 +64,22 @@ class FileScanner(object):
             return False
         if self.__bSave:
             try:
-                self.__file = open(self.__saveFile,"w")
+                self.__file = open(self.__saveFile,"w",encoding="utf-8")
             except IOError:
                 print("file: {0} open fail.".format(self.__file))
                 return False
+        
+        self.__filenameFound = []
         return True
 
 
     def __afterScan(self):
         if self.__bSave and self.__file:
+            #self.__file.seek(0)
+            self.__file.write("\n############  所有匹配的文件  ############\n")
+            for filename in self.__filenameFound:
+                self.__file.write("{0}\n".format(filename))
+            self.__file.write("#########################################\n\n")
             self.__file.close()
 
 
@@ -76,14 +92,16 @@ class FileScanner(object):
         charcode = ret["encoding"]
         file = open(filepath,"r",encoding=charcode)
         
-
         lineNum = 0
         bFirstFound = False
         for line in file:
             lineNum = lineNum + 1
             if re.search(self.__patternStr, line):
                 self.__process(filepath, lineNum,line,bFirstFound)
+                if not bFirstFound:
+                    self.__filenameFound.append(filepath)
                 bFirstFound = True
+        file.close()
     
 
     def __scanDir(self,dirpath):
@@ -106,14 +124,21 @@ class FileScanner(object):
             if self.__file:
                 if not bFirstFound:
                     self.__file.write("\n########################################################################################################################\n")
-                    self.__file.write("\nFound result of ==> {0}\n".format(filepath))
-                self.__file.write("lineNumber [{0}]\nlineContent : {1}".format(lineNum,line))
+                    self.__file.write("\n文件 ==> {0}\n".format(filepath))
+                if self.__bPrintLineNum:
+                    self.__file.write("[{0}]\n".format(lineNum))
+                if self.__bPrintLineContent:
+                    self.__file.write("{0}\n".format(line))
         else:
             #输出的屏幕
             if not bFirstFound:
                 print("\n########################################################################################################################\n")
-                print("Found result of ==> {0}\n".format(filepath))
-            print("lineNumber [{0}]\nlineContent : {1}".format(lineNum,line))
+                print("\n文件 ==> {0}\n".format(filepath))
+            if self.__bPrintLineNum:
+                print("[{0}]\n".format(lineNum))
+            if self.__bPrintLineContent:
+                print("{0}\n".format(line))
+            
         
 
 
@@ -126,4 +151,6 @@ if __name__ == "__main__":
     # scanner.scan()
 
     scanner = FileScanner("C:/Users/smallpi/Desktop/NCUtilTest/20160306/","""^POLYLINE""",True,"C:/Users/smallpi/Desktop/scan_result.txt")
+    #scanner.setPrintLineNum(False)
+    scanner.setPrintLineContent(False)
     scanner.scan()
